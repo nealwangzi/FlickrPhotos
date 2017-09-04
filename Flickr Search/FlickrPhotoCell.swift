57f7ea2cad97d4f2c08223cd226865abfdb9373e
neal <caoyaha@cloudoc.cn>
2017-09-04 15:05:39 +0800

    Add collectionview 基本使用 ； 第一部分展示搜索结果缩略图

---
+++ Flickr Search/FlickrPhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrPhotoCell.swift
+//  Flickr Search
+//
+//  Created by neal on 2017/9/4.
+//  Copyright © 2017年 neal. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
